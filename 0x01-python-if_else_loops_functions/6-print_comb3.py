ffe8afec3e8887fd8281824cfaefd7c848a005f5
TheOneNyangweso <morurisammy5@gmail.com>
2022-12-01 04:55:10 +0000

    "6-print_comb3.py"

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 for a in range(0, 10, 1):
-    for b in range(0,10,1):
+    for b in range(0, 10, 1):
         if (a == b):
             continue
-        elif(a > b):
+        elif (a > b):
             continue
         elif (a != 8 and a != 9):
-            print("{}{}, ".format(a,b), end='')
+            print("{}{}, ".format(a, b), end='')
         else:
-            print("{}{}".format(a,b))
-
+            print("{}{}".format(a, b))
