68174d9b4e33047cb3c14b43d1a45a7333a7fca4
TheOneNyangweso <morurisammy5@gmail.com>
2022-12-21 19:46:49 +0000

    "2-args.py"

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     n = len(argv)
 
     if n == 1:
-        print("0 arguments")
+        print("0 arguments.")
     elif n == 2:
         print("{} argument:".format(n - 1))
         for i in range(1, n):
