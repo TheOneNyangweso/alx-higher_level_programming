4e28df4e5b4a1ff5a74cd1634141bc1cb8118cf7
TheOneNyangweso <morurisammy5@gmail.com>
2022-12-22 13:17:16 +0000

    "12-fizzbuzz.py"

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    for i in range(1,101,1):
-        if (i % 3 == 0) and (i % 5 == 0):
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print("{}".format(i), end=" ")
+def fizzbuzz():
+    if __name__ == "__main__":
+        for i in range(1, 101, 1):
+            if (i % 3 == 0) and (i % 5 == 0):
+                print("FizzBuzz", end=" ")
+            elif i % 3 == 0:
+                print("Fizz", end=" ")
+            elif i % 5 == 0:
+                print("Buzz", end=" ")
+            else:
+                print("{}".format(i), end=" ")
