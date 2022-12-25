59c3b4bfe049ac13460196acd28cba0b508e3c9c
TheOneNyangweso <morurisammy5@gmail.com>
2022-12-25 13:06:34 +0000

    "100-my_calculator.py"

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """ A basic calculator taking arguments from CLI"""
+    import sys
     from sys import argv
     from calculator_1 import add, sub, mul, div
 
-    n = len(argv)
-    opr = ['+', '-', '*', '/']
+    opr = {"+": add, "-": sub, "*": mul, "/": div}
     a = int(argv[1])
     b = int(argv[3])
 
-    if n != 4:
-        print(argv[1])
-        print(n)
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     else:
-        if argv[2] == opr[0]:
-            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], add(a, b)))
-            exit(0)
-        elif argv[2] == opr[1]:
-            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], sub(a, b)))
-            exit(0)
-        elif argv[2] == opr[2]:
-            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], mul(a, b)))
-            exit(0)
-        elif argv[2] == opr[3]:
-            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], div(a, b)))
-            exit(0)
+        if argv[2] in  list(opr.keys()):
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], opr[argv[2]](a, b)))
+            sys.exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+            sys.exit(1)
