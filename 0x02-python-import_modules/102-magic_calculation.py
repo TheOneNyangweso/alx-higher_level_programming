739525329de59f44e1d2ee5244065da1a6f3a23e
TheOneNyangweso <morurisammy5@gmail.com>
2022-12-25 13:12:09 +0000

    "100-my_calculator.py"

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,16 +7,15 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     opr = {"+": add, "-": sub, "*": mul, "/": div}
-    a = int(argv[1])
-    b = int(argv[3])
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
-        if argv[2] in  list(opr.keys()):
-            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], opr[argv[2]](a, b)))
-            sys.exit(0)
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] in list(opr.keys()):
+            print("{} {} {} = {}".format(a, argv[2], b, opr[argv[2]](a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
