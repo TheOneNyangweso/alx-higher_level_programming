2b70989a45027d6d6c56a37be67fa59d9f330a88
TheOneNyangweso <morurisammy5@gmail.com>
2024-05-23 20:25:52 +0300

    ..

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,5 @@
 --1-create_database_if_missing - script to create database
--- Create a new database called 'hbtn_0c_0'
--- Create the new database if it does not exist already
+-- Create a new database called 'hbtn_0c_0' if it does not exist already
 CREATE DATABASE
+-- check if it exists
 IF NOT EXISTS hbtn_0c_0;
