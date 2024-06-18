63d3a5f1c182ea210abb68bc706264d0a5d8c12f
TheOneNyangweso <morurisammy5@gmail.com>
2024-06-18 21:51:34 +0300

    3 bug fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const restOfArgs = argv.slice(2);
-if (restOfArgs[0] == null || restOfArgs == undefined) {
+if (restOfArgs[0] == null || restOfArgs[0] == undefined) {
   console.log('No argument');
 } else {
   console.log(restOfArgs[0]);
