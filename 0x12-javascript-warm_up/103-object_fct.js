bbcce04082e64a629b369f66adc952dfcf50d5af
TheOneNyangweso <morurisammy5@gmail.com>
2024-06-20 02:54:34 +0300

    I wonder

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,8 +4,8 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function incr () {
-  this.value += 1;
+myObject.incr = function () {
+  this.value++;
 };
 myObject.incr();
 console.log(myObject);
