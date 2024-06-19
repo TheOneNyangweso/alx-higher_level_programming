2c8933aa5d992af0678e4c8f65934bea44dd27ca
TheOneNyangweso <morurisammy5@gmail.com>
2024-06-20 02:57:01 +0300

    added #!/usr/bin/node to files

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,5 @@
-exports.callMeMoby = function callMeMoby (x, callbackfn) {
+#!/usr/bin/node
+exports.callMeMoby = function callMeMoby(x, callbackfn) {
   let initial = 0;
   for (initial; initial < x; initial++) {
     const func = callbackfn;
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,3 +1,4 @@
-exports.addMeMaybe = function addMeMaybe (number, callbackfn) {
+#!/usr/bin/node
+exports.addMeMaybe = function addMeMaybe(number, callbackfn) {
   callbackfn(number + 1);
 };
